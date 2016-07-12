cf2ec6d597ba2a22ed41a299a542a31788199fe8
Cantoraz Chou <cantoraz@gmail.com>
2016-07-12 12:24:42 +0800

    Initial Commit

---
+++ CALayerAnimationGradient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CALayerAnimationGradient
+//
+//  Created by Cantoraz Chou on 7/12/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
